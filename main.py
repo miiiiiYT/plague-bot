f1a8e441e4f91eac3d835a8dac52ba81514b046f
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2021-07-19 14:53:20 +0200

    Added main file

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import discord 
+# This example requires the 'members' privileged intents
+
+import discord
+from discord.ext import commands
+import random
+from .setup import setup
+
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+bot.run('ODY2NjQ0NDk0ODc0NTc0ODc5.YPVjlQ.WmaWuzM5dHm8U84srSlGMt69fkA')
